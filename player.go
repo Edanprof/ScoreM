f21b909538d747b88839b1956a0b83645cbc4450
Ed <edenpoel@gmail.com>
2016-02-13 14:58:06 +0900

    add player struct

---
+++ player.go
@@ -0,0 +1,20 @@
+package player
+
+type PlayerInfo struct{
+	name string
+	nick string
+	brith int
+	height int
+	weight int
+
+	nation string
+	team string
+}
+
+type PlayerStatus struct{
+	status string
+	goal int
+	asis int
+	foul int
+	inj string
+}
