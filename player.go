6bb05c466b79ec231058c36f025874b7facb9e2d
Ed <edenpoel@gmail.com>
2016-02-13 16:10:48 +0900

    updated player and team struct

--- player.go
+++ player.go
@@ -1,18 +1,20 @@
 package player
 
 type PlayerInfo struct{
-	name string
-	nick string
-	brith int
-	height int
-	weight int
+	name string // 이름
+	nick string // 별명
+	brith int // 생년월일
+	height int // 키cm
+	weight int // 몸무게 kg
+	position map[string]int{} // 포지션, 포지션별 숙련도
 
-	nation string
-	team string
+	nation string // 소속국가
+	team string // 소속팀
 }
 
 type PlayerStatus struct{
 	status string
+	first boolean
 	goal int
 	asis int
 	foul int
